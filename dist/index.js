b1da86a716f2312a4b403b422f98c4260626d60f
Yuvraj <thunderyuvi911@gmail.com>
2024-06-30 13:16:06 +0530

    Functions Object Classes Type_Assertion

--- dist/index.js
+++ dist/index.js
@@ -18,11 +18,12 @@ const func1 = (n, m) => {
     console.log(n, m);
     return n * m;
 };
+const mode = "light";
 // Array
-//Method-1
+//Method-1)
 const arr = [10, 20, 30, 40];
 const arr1 = ['a', 'b', 'c', 'd'];
-//Method-2
+//Method-2)
 const arr2 = [];
 const arr3 = [];
 //if i want both number and string
@@ -64,5 +65,144 @@ const obj5 = {
         console.log(n * m);
     }
 };
-// obj5?.funct(10,20)
-//Functions
+const func2 = (n, m, l) => {
+    //type guard
+    if (typeof l === "undefined")
+        return "l is not provided";
+    return n * m * l;
+};
+func2(20, 10);
+func2(20, 5, 10);
+//Default Parameters
+const func3 = (n, m, l = 10) => {
+    return n * m * l;
+};
+func3(10, 10);
+const func4 = (...m) => {
+    return m;
+};
+func4(1, 2, 3, 4, 5, 6, 7);
+function yuvi(n) {
+    return 50;
+}
+const yuvi1 = function yuvi1(...n) {
+    return n;
+};
+//Functions with Object
+//Method-1)
+const getData = (product) => {
+    console.log(product);
+};
+const getData1 = (product) => {
+    console.log(product);
+};
+const productOne = {
+    name: "Macbook",
+    stock: 20,
+    price: 81999,
+    photo: "samplephotourl",
+};
+getData(productOne);
+const getData2 = (product) => {
+    console.log(product);
+};
+const productOne1 = {
+    name: "Macbook",
+    stock: 20,
+    price: 81999,
+    photo: "samplephotourl",
+    id: "yuvraj"
+};
+getData(productOne);
+//Never Type => jab hum error throw krte h toh return type never hota h.
+const errorHandler = () => {
+    throw new Error();
+};
+// Classes = javascript me access specifiers nhi hote lekin typescript me c++ ki trh 3 access specifiers h. Isme by-default public hote h.
+class Player {
+    constructor(height, weight, power) {
+        this.height = height;
+        this.weight = weight;
+        this.power = power;
+        this.id = String(Math.random() * 100);
+    }
+    // myHeight = () => {
+    //     return this.height;
+    // }
+    //upar vala code aur ye line dono same h.
+    // myHeight = () => this.height;
+    //getter => ye hamesha 1 value return krega
+    get getMyHeight() {
+        return this.height;
+    }
+    //setter => ye hamesha parameter me keval 1 value lega.
+    set changeHeight(val) {
+        this.height = val;
+    }
+}
+const yuvi2 = new Player(15, 50, 100);
+//hum isko run krege toh hume console me 50 mil jaiye ga kyoki private keyword keval ts me h js me nhi aur hmari ts ka code js me convert hota h aur phir browser pr jata h isliye error nhi aaye ga.
+// console.log(yuvi2.height);
+console.log(yuvi2.weight);
+// console.log(yuvi2.myHeight());
+//setter aur getter function ko hum call nhi krte h hum directly variable ki trh access krte h.
+console.log(yuvi2.getMyHeight);
+yuvi2.changeHeight = 10;
+console.log(yuvi2.getMyHeight);
+class Player2 extends Player {
+    constructor(height, weight, power, special) {
+        super(height, weight, power);
+        this.getMyPower = () => this.power;
+        this.special = special;
+    }
+}
+const yuvi3 = new Player2(15, 50, 100, true);
+console.log(yuvi3.weight);
+// console.log(yuvi3.myHeight());
+console.log(yuvi3.getMyPower());
+console.log(yuvi3.id);
+class Product1 {
+    constructor(name, price, stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        // public name: string;
+        // public price: number;
+        // public stock: number;
+        this.member = true;
+        this.getMember = () => this.member;
+        //    this.name = name;
+        //    this.price = price;
+        //    this.stock = stock; 
+    }
+}
+const prod1 = new Product1("Macbook", 200000, 20);
+prod1.getMember();
+//TYPE ASSERTION = ye ek technique h jiski help se hum compiler ko btate h ki hume variable ka type pta h. Ye type checking ko override krne k kaam aati h. Type assertion do tarikon se ki ja sakti hai: "angle-bracket" syntax ya "as" syntax.
+// const btn = document.getElementById("btn") as HTMLElement;
+const btn = document.getElementById("btn");
+btn.onclick;
+//yadi hum type assertion nhi dete h toh hume ? lgana hota h
+// btn?.onclick
+//hum ek tarika aue use kr skte h jisme hum btaye ge ki vo kuch bhi value ho skti h lekin null nhi hogi.
+const btn1 = document.getElementById("btn");
+btn1.onclick;
+// const img = document.getElementById("myimg") as HTMLImageElement;
+const img = document.querySelector("img");
+img.src;
+const form = document.getElementById("myform");
+const myinput = document.querySelector("form > input");
+form.onsubmit = (e) => {
+    e.preventDefault();
+    const value = Number(myinput.value);
+    console.log(value);
+    console.log(typeof value);
+    const h2 = document.createElement("h2");
+    h2.textContent = String(value + 20);
+    const body = document.querySelector("body");
+    body.append(h2);
+};
+const myObj = {
+    name: "Yuvraj",
+    email: "yuvraj091102@gmail.com"
+};
--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,14 @@ type MyName = string | number;
 let c:MyName = "yuvraj";
 let d:MyName = 75;
 
-type UserName=(
-    n: number,
-    m: number
-) => number ;
+type UserName=( n: number, m: number) => number ;
 const func1: UserName = (n,m) => {
     console.log(n,m);
     return n*m;
 }
+
+type themeMode = "light" | "dark";
+const mode:themeMode = "light";
 
 // Array
 //Method-1)
@@ -104,3 +104,220 @@ const obj5: NewObj = {
 // obj5?.funct(10,20)
 
 //Functions
+type FuncType1 = (n: number, m:number, l?: number) => number | string;
+const func2: FuncType1 = (n, m, l) => {
+    //type guard
+    if(typeof l === "undefined") return "l is not provided";
+
+    return n*m*l;
+}
+func2(20,10);
+func2(20,5,10); 
+
+//Default Parameters
+const func3:FuncType1 = (n, m, l=10) => {
+    return n*m*l;
+}
+func3(10,10);
+
+//Rest Operator=> yadi hume ye nhi pta ki kitne no parameter me aaye ge function me toh hum rest operator ka use kr skte h. Iski help se hum sbhi no ko ek array k roop me accept kr skte h.
+// const func4 = (...m:number[])=>{
+    //     return m;
+    // }
+type FuncType2 = (...m:number[]) => number[];
+const func4:FuncType2 = (...m)=>{
+    return m;
+}
+func4(1,2,3,4,5,6,7);
+
+function yuvi(n:number):number{
+    return 50;
+}
+
+const yuvi1:FuncType2 = function yuvi1(...n){
+    return n;
+}
+
+//Functions with Object
+//Method-1)
+const getData = (product:{name: string, stock:number, price: number, photo:string}) : void => {
+    console.log(product);
+}
+
+// Method-2)
+type GetDataType = (product:{
+    name:string;
+    stock: number;
+    price: number;
+    photo: string;
+}) => void;
+const getData1: GetDataType = (product) => {
+    console.log(product);
+}
+const productOne: {
+    name:string;
+    stock: number;
+    price: number;
+    photo: string;
+} = {
+    name: "Macbook",
+    stock: 20,
+    price: 81999,
+    photo: "samplephotourl",
+}
+getData(productOne);
+
+// Method-3)
+// hum ishe interface ki jagah type ki help se bhi bna skte h jaise ki type Product = {}
+interface Product {
+    name:string;
+    stock: number;
+    price: number;
+    photo: string;
+    //humne ek baar jo value de di hamesa vo hi rhi ge hum ushe change nhi kr skte.
+    readonly id: string;
+}
+type GetDataType1 = (product:Product) => void;
+const getData2: GetDataType = (product) => {
+    console.log(product);
+}
+const productOne1: Product = {
+    name: "Macbook",
+    stock: 20,
+    price: 81999,
+    photo: "samplephotourl",
+    id: "yuvraj"
+}
+getData(productOne);
+
+//Never Type => jab hum error throw krte h toh return type never hota h.
+const errorHandler = (): never=>{
+    throw new Error();
+}
+
+
+// Classes = javascript me access specifiers nhi hote lekin typescript me c++ ki trh 3 access specifiers h. Isme by-default public hote h.
+class Player {
+    // private height;
+    // public weight;
+    // protected power;
+    // constructor(height: number, weight: number){
+    //     this.height = height;
+    //     this.weight = weight;
+    // }
+
+    //ye code upar commented code k equal h.
+    readonly id:string
+    constructor (private height: number, public weight: number, protected power: number){
+        this.id = String(Math.random()*100)
+    }
+
+    // myHeight = () => {
+    //     return this.height;
+    // }
+    //upar vala code aur ye line dono same h.
+    // myHeight = () => this.height;
+
+    //getter => ye hamesha 1 value return krega
+    get getMyHeight():number{
+        return this.height;
+    }
+    //setter => ye hamesha parameter me keval 1 value lega.
+    set changeHeight(val: number){
+        this.height = val;
+    } 
+}
+
+const yuvi2 = new Player(15, 50, 100);
+//hum isko run krege toh hume console me 50 mil jaiye ga kyoki private keyword keval ts me h js me nhi aur hmari ts ka code js me convert hota h aur phir browser pr jata h isliye error nhi aaye ga.
+// console.log(yuvi2.height);
+console.log(yuvi2.weight);
+// console.log(yuvi2.myHeight());
+//setter aur getter function ko hum call nhi krte h hum directly variable ki trh access krte h.
+console.log(yuvi2.getMyHeight);
+yuvi2.changeHeight = 10;
+console.log(yuvi2.getMyHeight);
+
+
+class Player2 extends Player{
+    special: boolean;
+    constructor(height: number, weight: number, power: number, special: boolean){
+        super(height, weight, power);
+        this.special = special;
+    }
+    getMyPower = () => this.power;
+}
+const yuvi3 = new Player2(15, 50, 100, true);
+console.log(yuvi3.weight);
+// console.log(yuvi3.myHeight());
+console.log(yuvi3.getMyPower());
+console.log(yuvi3.id);
+
+//Interface with class
+
+interface ProductType {
+    name: string;
+    price: number;
+    stock: number;
+    // getMember: () => boolean;
+    offer?: boolean;
+}
+interface GiveId {
+    getMember: () => boolean;
+}
+class Product1 implements ProductType, GiveId{
+    // public name: string;
+    // public price: number;
+    // public stock: number;
+    private member: boolean = true;
+
+    constructor(public name: string, public price: number, public stock:number){
+    //    this.name = name;
+    //    this.price = price;
+    //    this.stock = stock; 
+    }
+
+    getMember = () => this.member;
+}
+const prod1 = new Product1("Macbook", 200000, 20);
+prod1.getMember();
+
+
+//TYPE ASSERTION = ye ek technique h jiski help se hum compiler ko btate h ki hume variable ka type pta h. Ye type checking ko override krne k kaam aati h. Type assertion do tarikon se ki ja sakti hai: "angle-bracket" syntax ya "as" syntax.
+// const btn = document.getElementById("btn") as HTMLElement;
+const btn = <HTMLElement>document.getElementById("btn");
+btn.onclick;
+//yadi hum type assertion nhi dete h toh hume ? lgana hota h
+// btn?.onclick
+
+//hum ek tarika aue use kr skte h jisme hum btaye ge ki vo kuch bhi value ho skti h lekin null nhi hogi.
+const btn1 = document.getElementById("btn")!;
+btn1.onclick;
+
+// const img = document.getElementById("myimg") as HTMLImageElement;
+const img = document.querySelector("img")!;
+img.src;
+
+const form = document.getElementById("myform") as HTMLFormElement;
+const myinput = document.querySelector("form > input") as HTMLInputElement;
+form.onsubmit=(e: SubmitEvent)=>{
+    e.preventDefault();
+    const value = Number(myinput.value)
+    console.log(value);
+    console.log(typeof value);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = String(value + 20);
+    const body = document.querySelector("body")!;
+    body.append(h2)
+}
+
+
+interface Person{
+    name: string,
+    email: string,
+}
+const myObj: Person = {
+    name: "Yuvraj",
+    email: "yuvraj091102@gmail.com"
+}
